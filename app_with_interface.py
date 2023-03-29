617fc069d7a43abf29c6b6d674a3e897b6b3b105
ja818 <julianarango1818@gmail.com>
2023-03-28 22:57:15 -0500

    Moved model to separated file

--- app_with_interface.py
+++ app_with_interface.py
@@ -1,27 +1,9 @@
 import gradio as gr
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from model import predict
 
 # Main app function
 def model_predict(sentence):
     prediction = predict(sentence)
-    return prediction
-
-# Model logic
-def predict(user_input):
-    sid_obj = SentimentIntensityAnalyzer()
-    sentiment_dict = sid_obj.polarity_scores(user_input)
-    prediction = ""
-    
-    # Stablish prediction according to polarity score
-    if sentiment_dict['compound'] >= 0.05 :
-        prediction = "Positive"
-
-    elif sentiment_dict['compound'] <= - 0.05 :
-        prediction = "Negative"
-
-    else :
-        prediction = "Neutral"  
-
     return prediction
 
 # Create gradio app structure
