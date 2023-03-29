13db3a5312fbb47cd0fd47f11261eb434e4ad92b
ja818 <julianarango1818@gmail.com>
2023-03-28 22:43:29 -0500

    Uploaded model logic

---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Model logic
+def predict(user_input):
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores(user_input)
+    prediction = ""
+    
+    # Stablish prediction according to polarity score
+    if sentiment_dict['compound'] >= 0.05 :
+        prediction = "Positive"
+
+    elif sentiment_dict['compound'] <= - 0.05 :
+        prediction = "Negative"
+
+    else :
+        prediction = "Neutral"  
+
+    return prediction
